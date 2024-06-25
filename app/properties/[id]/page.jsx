600c98f3498422e60d23c2748d26e595cf0baa4c
Abhinav Gupta <gabhinav95a@gmail.com>
2024-06-25 21:51:15 +0530

    client components and router hooks

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -1,5 +1,32 @@
+"use client";
+
+import {
+  useParams,
+  useSearchParams,
+  useRouter,
+  usePathname,
+} from "next/navigation";
+
 const PropertyPage = () => {
-  return <span className="font-medium text-xl">Property page</span>;
+  const { id } = useParams();
+  const searchParams = useSearchParams();
+  const router = useRouter();
+  const name = searchParams.get("name");
+  const pathName = usePathname();
+  console.log(pathName);
+  return (
+    <div className="flex flex-col m-5 justify-around">
+      <span className="font-medium text-xl">Property page</span>
+      <span className="text-lg">Property id: {id}</span>
+      <span className="text-lg">Property name: {name}</span>
+      <button
+        onClick={() => router.push("/")}
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded max-w-36"
+      >
+        Go home {pathName}
+      </button>
+    </div>
+  );
 };
 
 export default PropertyPage;
